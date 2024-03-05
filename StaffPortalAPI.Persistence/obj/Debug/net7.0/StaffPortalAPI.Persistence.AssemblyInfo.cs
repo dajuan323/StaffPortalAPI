350af8952bcc18d76d845a5e28bd598a93b3f049
dajuan323 <macaro.martin@gmail.com>
2024-03-04 23:18:48 -0500

    Setting up remote branch for caching

--- StaffPortalAPI.Persistence/obj/Debug/net7.0/StaffPortalAPI.Persistence.AssemblyInfo.cs
+++ StaffPortalAPI.Persistence/obj/Debug/net7.0/StaffPortalAPI.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StaffPortalAPI.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2432bf8e94f76a00d0e4abba092e65316a145a57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71db942fc76267d3c647dde54d93dc1ac8812518")]
 [assembly: System.Reflection.AssemblyProductAttribute("StaffPortalAPI.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StaffPortalAPI.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
